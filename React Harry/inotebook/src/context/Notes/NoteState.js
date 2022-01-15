ad2e89b8a433cdccfc0243c09ed6d2634cae743f
mehraj43 <mehrajmalik43@gmail.com>
2022-01-15 15:48:10 +0530

    Video 60 completed

--- React Harry/inotebook/src/context/Notes/NoteState.js
+++ React Harry/inotebook/src/context/Notes/NoteState.js
@@ -2,23 +2,30 @@ import NoteContext from './noteContext';
 import { useState } from 'react';
 
 const NoteState = (props) => {
-  const s1 = {
-    name: 'Harry',
-    class: '5b',
-  };
+  const notesInitial = [
+    {
+      _id: '61e12669a70377604e25115e',
+      user: '61e125d9a70377604e251158',
+      title: 'South Movies',
+      description: 'Going to watch all movies',
+      tag: 'South',
+      date: '2022-01-14T07:29:45.024Z',
+      __v: 0,
+    },
+    {
+      _id: '61e12673a70377604e251160',
+      user: '61e125d9a70377604e251158',
+      title: 'Pushpa',
+      description: "I'm gonna watch it",
+      tag: 'In Mobile',
+      date: '2022-01-14T07:29:55.285Z',
+      __v: 0,
+    },
+  ];
 
-  const [state, setState] = useState(s1);
-  const update = () => {
-    setTimeout(() => {
-      setState({
-        name: 'Meraj',
-        class: '10B',
-      });
-    }, 2000);
-  };
-
+  const [notes, setNotes] = useState(notesInitial);
   return (
-    <NoteContext.Provider value={{ state, update }}>
+    <NoteContext.Provider value={{ notes, setNotes }}>
       {props.children}
     </NoteContext.Provider>
   );
