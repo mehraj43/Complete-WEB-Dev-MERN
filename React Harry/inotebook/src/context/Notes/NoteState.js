7497a16b749cb6c808ad67916060e4832e23b552
mehraj43 <mehrajmalik43@gmail.com>
2022-01-17 14:06:53 +0530

    video 63 completed

--- React Harry/inotebook/src/context/Notes/NoteState.js
+++ React Harry/inotebook/src/context/Notes/NoteState.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 const NoteState = (props) => {
   const notesInitial = [
     {
-      _id: '61e12669a70377604e25115e',
+      _id: '61e126659a70377604e25115e',
       user: '61e125d9a70377604e251158',
       title: 'South Movies',
       description: 'Going to watch all movies',
@@ -13,7 +13,52 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: '61e12673a70377604e251160',
+      _id: '61e12673a70377604e6251160',
+      user: '61e125d9a70377604e251158',
+      title: 'Pushpa',
+      description: "I'm gonna watch it",
+      tag: 'In Mobile',
+      date: '2022-01-14T07:29:55.285Z',
+      __v: 0,
+    },
+    {
+      _id: '61e12673a750377604e251160',
+      user: '61e125d9a70377604e251158',
+      title: 'Pushpa',
+      description: "I'm gonna watch it",
+      tag: 'In Mobile',
+      date: '2022-01-14T07:29:55.285Z',
+      __v: 0,
+    },
+    {
+      _id: '61e12673a70377604e7251160',
+      user: '61e125d9a70377604e251158',
+      title: 'Pushpa',
+      description: "I'm gonna watch it",
+      tag: 'In Mobile',
+      date: '2022-01-14T07:29:55.285Z',
+      __v: 0,
+    },
+    {
+      _id: '61e12673a570377604e251160',
+      user: '61e125d9a70377604e251158',
+      title: 'Pushpa',
+      description: "I'm gonna watch it",
+      tag: 'In Mobile',
+      date: '2022-01-14T07:29:55.285Z',
+      __v: 0,
+    },
+    {
+      _id: '61e12673a703776704e251160',
+      user: '61e125d9a70377604e251158',
+      title: 'Pushpa',
+      description: "I'm gonna watch it",
+      tag: 'In Mobile',
+      date: '2022-01-14T07:29:55.285Z',
+      __v: 0,
+    },
+    {
+      _id: '61e12673a703677604e251160',
       user: '61e125d9a70377604e251158',
       title: 'Pushpa',
       description: "I'm gonna watch it",
@@ -24,8 +69,30 @@ const NoteState = (props) => {
   ];
 
   const [notes, setNotes] = useState(notesInitial);
+
+  // Add a Note
+  const addNote = (title, description, tag) => {
+    console.log('Adding Note');
+    const note = {
+      _id: '61e12673a703677604e25g1160',
+      user: '61e125d9a70377604e251158',
+      title: title,
+      description: description,
+      tag: tag,
+      date: '2022-01-14T07:29:55.285Z',
+      __v: 0,
+    };
+    setNotes(notes.concat(note));
+  };
+
+  // Delete a Note
+  const deleteNote = (id) => {};
+
+  // Edit a Note
+  const editNote = (id) => {};
+
   return (
-    <NoteContext.Provider value={{ notes, setNotes }}>
+    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
       {props.children}
     </NoteContext.Provider>
   );
